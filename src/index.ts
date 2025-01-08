0c56593e059dfcb90b0791de497b480d4d53d21e
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-01-08 12:42:59 -0800

    fix(exports): add missing type export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { buildToHaveStyleRule, type Options, type Value } from './toHaveStyleRule.js';
+export { buildToHaveStyleRule, type Options, type ToHaveStyleRuleMatcher, type Value } from './toHaveStyleRule.js';
 export { resetStyleSheet } from './utils.js';
